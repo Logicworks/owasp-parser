cff1d1c43b864d2fb94f5deb31cac5eeae769a78
Phil Christensen <phil@bubblehouse.org>
2015-11-08 17:51:52 -0500

    poc essentially done, but there's not a lot that the AWS WAF can do right now

--- parser.py
+++ parser.py
@@ -4,11 +4,76 @@ import shlex
 
 logging.basicConfig(level=logging.DEBUG)
 
-log = logging.getLogger(__name__ if __name__ != '__main__' else 'owasp-parser')
+class OwaspRule(object):
+    def __init__(self, entry, log=None):
+        parts = shlex.split(entry)
+        self.log = log if log else logging.getLogger('rule')
+        self.variables = parts[1].split('|')
+        self.operator = parts[2]
+        self.actions = parts[3] if len(parts) == 4 else None
+    
+    def export(self):
+        result = []
+        for var in self.variables:
+            selection = None
+            exclude = False
+            count = False
+            if(':' in var):
+                var, selection = var.split(':')
+            if(var.startswith('!')):
+                var, exclude = var[1:], True
+            elif(var.startswith('&')):
+                var, count = var[1:], True
+            checker = getattr(self, 'check_%s' % var.lower(), None)
+            if not(callable(checker)):
+                self.log.warning("Unsupported variable: %s" % var)
+                continue
+            try:
+                result.append(checker(selection, exclude, count))
+            except NotImplementedError, e:
+                self.log.warning(str(e))
+                continue
+        return result
+
+class AmazonWafRule(OwaspRule):
+    def __init__(self, entry):
+        super(AmazonWafRule, self).__init__(entry, logging.getLogger('waf'))
+    
+    def apply_operator(self, value):
+        if not(self.operator.startswith('@')):
+            raise NotImplementedError("Unsupported operator: regex")
+        
+        args = self.operator.split()
+        op = getattr(self, "op_%s" % args[0][1:], None)
+        if not(callable(op)):
+            raise NotImplementedError("Unsupported operator: %s" % self.operator)
+            return
+        
+        return op(args[1:], value)
+    
+    def op_beginsWith(self, args, value):
+        return 'headerBeginsWith(%r, %r)' % (value, args)
+    
+    def op_eq(self, args, value):
+        return 'headerEquals(%r, %r)' % (value, args)
+    
+    def check_request_headers(self, selection=None, exclude=False, count=False):
+        if(count):
+            self.log.warning("Unsupported check: variable counts")
+        
+        return self.apply_operator(selection)
+    
+    def check_request_uri(self, selection=None, exclude=False, count=False):
+        if(count):
+            self.log.warning("Unsupported check: variable counts")
+        
+        return self.apply_operator(selection)
 
 class OwaspAcl(object):
-    def __init__(self, filename):
+    def __init__(self, filename, rule_factory=OwaspRule):
+        self.log = logging.getLogger('acl')
         self.filename = filename
+        self.rule_factory = rule_factory
         self._unparsed_rules = None
         self._rules = None
     
@@ -28,7 +93,7 @@ class OwaspAcl(object):
                 if(test.startswith('SecRule')):
                     current = test
                 elif(current.endswith('\\')):
-                    current = current[:-1] + test
+                    current = current[:-1] + ' ' + test
                 elif(current != ''):
                     result.append(current)
                     current = ''
@@ -41,19 +106,18 @@ class OwaspAcl(object):
         
         self._rules = result = []
         for entry in self.unparsed_rules:
-            sr, variables, operator, actions = shlex.split(entry)
-            rule = OwaspRule(variables, operator, actions)
-            result.append(rule)
+            try:
+                rule = self.rule_factory(entry)
+                result.append(rule)
+            except:
+                self.log.error('Malformed rule: %s' % entry)
         return result
 
-class OwaspRule(object):
-    def __init__(self, variables, operator, actions):
-        pass
-    
-    def __repr__(self):
-        return ''
-
 if __name__ == '__main__':
-    acl = OwaspAcl(sys.argv[1])
+    import json
+    
+    acl = OwaspAcl(sys.argv[1], rule_factory=AmazonWafRule)
     for rule in acl.rules:
-        print rule
+        result = rule.export()
+        if(result):
+            print '\n'.join([x for x in result if x])

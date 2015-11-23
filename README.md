9184fc5ab6ded448e89c4682141749ecfb512c36
Phil Christensen <pchristensen@logicworks.net>
2015-11-23 11:27:04 -0500

    added sample output

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ owasp-parser
 =============
 
 Parse the mod_security ruleset and export into other formats.
+
+1. Check out the module and update the owasp-crs submodule.
+  1. `git submodule init`
+  1. `git submodule update`
+1. Run the parser.py script against a file
+1. `python parser.py owasp/base_rules/modsecurity_crs_20_protocol_violations.conf`
+
+This doesn't really do much of anything right now besides parse the rules and identify which ones we could conceivably translate to AWS WAF.
--- parser.py
+++ parser.py
@@ -5,12 +5,34 @@ import shlex
 logging.basicConfig(level=logging.DEBUG)
 
 class OwaspRule(object):
+    """
+    Take a single "SecRule" entry and parse it.
+    
+    This is an abstract base class, output-specific functions are in the AmazonWafRule subclass.
+    """
     def __init__(self, entry, log=None):
+        """
+        Given a quoted "SecRule" string, parse it into variables, operator, and actions
+        """
         parts = shlex.split(entry)
         self.log = log if log else logging.getLogger('rule')
         self.variables = parts[1].split('|')
         self.operator = parts[2]
-        self.actions = parts[3] if len(parts) == 4 else None
+        self._actions = parts[3] if len(parts) == 4 else None
+    
+    @property
+    def actions(self):
+        if(self._actions is None):
+            return None
+        actions = dict()
+        elements = shlex.split(self._actions.replace(',', ' '))
+        for item in elements:
+            if(':' in item):
+                key, value = item.split(':')
+            else:
+                key, value = item, None
+            actions[key] = value
+        return actions
     
     def export(self):
         result = []
@@ -24,7 +46,10 @@ class OwaspRule(object):
                 var, exclude = var[1:], True
             elif(var.startswith('&')):
                 var, count = var[1:], True
-            checker = getattr(self, 'check_%s' % var.lower(), None)
+            if(':' in var):
+                checker = getattr(self, 'check_%s' % var.split(':')[0].lower(), None)
+            else:
+                checker = getattr(self, 'check_%s' % var.lower(), None)
             if not(callable(checker)):
                 self.log.warning("Unsupported variable: %s" % var)
                 continue
@@ -39,35 +64,184 @@ class AmazonWafRule(OwaspRule):
     def __init__(self, entry):
         super(AmazonWafRule, self).__init__(entry, logging.getLogger('waf'))
     
-    def apply_operator(self, value):
-        if not(self.operator.startswith('@')):
-            raise NotImplementedError("Unsupported operator: regex")
-        
+    def apply_operator(self, value, variable_name):
         args = self.operator.split()
+        first_var = self.variables[0]
+        if not(self.operator.startswith('@')):
+            return self.op_regex(variable_name, args[1:], self.operator)
+        
         op = getattr(self, "op_%s" % args[0][1:], None)
         if not(callable(op)):
-            raise NotImplementedError("Unsupported operator: %s" % self.operator)
+            raise NotImplementedError("unsupported operator %s for %s:%r" % (args[0][1:], variable_name, value))
             return
         
-        return op(args[1:], value)
+        return op(variable_name, args[1:], value)
     
-    def op_beginsWith(self, args, value):
-        return 'headerBeginsWith(%r, %r)' % (value, args)
+    def op_regex(self, variable, args, value):
+        raise NotImplementedError('values in %r match regex %r' % (variable, value))
     
-    def op_eq(self, args, value):
-        return 'headerEquals(%r, %r)' % (value, args)
+    def op_beginsWith(self, variable, args, value):
+        return 'values in %r begin with %r' % (value, args)
+    
+    def op_beginsWith(self, variable, args, value):
+        return 'values in %r begin with %r' % (value, args)
+    
+    def op_endsWith(self, variable, args, value):
+        return 'values in %r end with %r' % (value, args)
+    
+    def op_contains(self, variable, args, value):
+        return 'values in %r contain the string %r' % (value, args)
+    
+    def op_eq(self, variable, args, value):
+        return 'numbers in %r are equal to %r' % (value, args)
+    
+    def op_streq(self, variable, args, value):
+        return 'strings in %r are equal to %r' % (variable, args)
+    
+    def op_validateByteRange(self, variable, args, value):
+        return 'values in %r are within the byte range %s' % (variable, args)
+    
+    def op_validateUrlEncoding(self, variable, args, value):
+        return 'URLs in %r are urlencoded properly.' % (variable,)
+    
+    def op_validateUtf8Encoding(self, variable, args, value):
+        return 'values in %r are utf8-encoded properly.' % (variable,)
+    
+    def op_verifyCC(self, variable, args, value):
+        return 'values in %r are valid credit cards.' % (variable,)
     
     def check_request_headers(self, selection=None, exclude=False, count=False):
-        if(count):
-            self.log.warning("Unsupported check: variable counts")
-        
-        return self.apply_operator(selection)
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "REQUEST_HEADERS")
+        ])
     
+    def check_response_headers_names(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "RESPONSE_HEADERS_NAMES")
+        ])
+
+    def check_response_headers(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "RESPONSE_HEADERS")
+        ])
+    
+    def check_request_headers_names(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "REQUEST_HEADERS_NAMES")
+        ])
+
+    def check_request_body(self, selection=None, exclude=False, count=False):
+        raise NotImplementedError(''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "REQUEST_BODY")
+        ]))
+
+    def check_response_body(self, selection=None, exclude=False, count=False):
+        raise NotImplementedError(''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "RESPONSE_BODY")
+        ]))
+
     def check_request_uri(self, selection=None, exclude=False, count=False):
-        if(count):
-            self.log.warning("Unsupported check: variable counts")
-        
-        return self.apply_operator(selection)
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "REQUEST_URI")
+        ])
+
+    def check_request_line(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "REQUEST_LINE")
+        ])
+
+    def check_request_protocol(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "REQUEST_PROTOCOL")
+        ])
+
+    def check_request_filename(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "REQUEST_FILENAME")
+        ])
+
+    def check_request_basename(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "REQUEST_BASENAME")
+        ])
+
+    def check_request_cookies(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "REQUEST_COOKIES")
+        ])
+    
+    def check_request_cookies_names(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "REQUEST_COOKIES_NAMES")
+        ])
+    
+    def check_request_method(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "REQUEST_METHOD")
+        ])
+
+    def check_query_string(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "QUERY_STRING")
+        ])
+
+    def check_files(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "FILES")
+        ])
+
+    def check_files_names(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "FILES_NAMES")
+        ])
+
+    def check_args(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "ARGS")
+        ])
+
+    def check_args_names(self, selection=None, exclude=False, count=False):
+        return ''.join([
+            'exclude if ' if exclude else 'ensure that ',
+            'count of  ' if count else '',
+            self.apply_operator(selection, "ARGS_NAMES")
+        ])
 
 class OwaspAcl(object):
     def __init__(self, filename, rule_factory=OwaspRule):
